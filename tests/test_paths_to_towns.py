da58cd8faf0b00cdc08062b618b3c0cb82e11250
Claudio Santos <c.santos@consultoriasigma.com.br>
2018-02-18 17:58:34 -0300

    dont find a path

--- tests/test_paths_to_towns.py
+++ tests/test_paths_to_towns.py
@@ -6,3 +6,9 @@ def test_find_path_atob():
     node_b = Node('B')
     node_a.connect(node_b, distance=5)
     assert find_path(node_a, node_b) == 5
+
+
+def test_dont_find_path_atob():
+    node_a = Node('A')
+    node_b = Node('B')
+    assert find_path(node_a, node_b) is None
